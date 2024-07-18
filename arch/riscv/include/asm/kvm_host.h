fbf8a9fcca05a1b554ac0d01b0c46fbb6263c435
Quan Zhou <zhouquan@iscas.ac.cn>
2024-07-18 10:49:13 +0800

    riscv: KVM: add basic support for host vs guest profiling
    For the information collected on the host side, we need to
    identify which data originates from the guest and record
    these events separately. This can be achieved by having
    KVM register perf callbacks.
    
    Signed-off-by: Quan Zhou <zhouquan@iscas.ac.cn>

--- arch/riscv/include/asm/kvm_host.h
+++ arch/riscv/include/asm/kvm_host.h
@@ -285,6 +285,12 @@ struct kvm_vcpu_arch {
 	} sta;
 };
 
+/* TODO: A more explicit approach might be needed here than this simple one */
+static inline bool kvm_arch_pmi_in_guest(struct kvm_vcpu *vcpu)
+{
+	return IS_ENABLED(CONFIG_GUEST_PERF_EVENTS) && !!vcpu;
+}
+
 static inline void kvm_arch_sync_events(struct kvm *kvm) {}
 static inline void kvm_arch_sched_in(struct kvm_vcpu *vcpu, int cpu) {}
 
