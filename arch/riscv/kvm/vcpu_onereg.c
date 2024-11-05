b77a459ed7ef9708c36fe6407e4067ba2d7d9bd5
Quan Zhou <zhouquan@iscas.ac.cn>
2024-11-05 14:52:35 +0800

    RISC-V: KVM: Allow Svvptc extension for Guest/VM
    Extend the KVM ISA extension ONE_REG interface to allow KVM user space
    to detect and enable Svvptc extension for Guest/VM.
    
    Signed-off-by: Quan Zhou <zhouquan@iscas.ac.cn>

--- arch/riscv/kvm/vcpu_onereg.c
+++ arch/riscv/kvm/vcpu_onereg.c
@@ -39,6 +39,7 @@ static const unsigned long kvm_isa_ext_arr[] = {
 	KVM_ISA_EXT_ARR(SSCOFPMF),
 	KVM_ISA_EXT_ARR(SSTC),
 	KVM_ISA_EXT_ARR(SVINVAL),
+	KVM_ISA_EXT_ARR(SVVPTC),
 	KVM_ISA_EXT_ARR(SVNAPOT),
 	KVM_ISA_EXT_ARR(SVPBMT),
 	KVM_ISA_EXT_ARR(ZACAS),
@@ -131,6 +132,7 @@ static bool kvm_riscv_vcpu_isa_disable_allowed(unsigned long ext)
 	case KVM_RISCV_ISA_EXT_SSCOFPMF:
 	case KVM_RISCV_ISA_EXT_SSTC:
 	case KVM_RISCV_ISA_EXT_SVINVAL:
+	case KVM_RISCV_ISA_EXT_SVVPTC:
 	case KVM_RISCV_ISA_EXT_SVNAPOT:
 	case KVM_RISCV_ISA_EXT_ZACAS:
 	case KVM_RISCV_ISA_EXT_ZAWRS:
