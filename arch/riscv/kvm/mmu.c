f0bef3c018e63145420864ce44992dee6231ca50
Quan Zhou <zhouquan@iscas.ac.cn>
2025-06-10 17:58:41 +0800

    RISC-V: KVM: Avoid re-acquiring memslot in  kvm_riscv_gstage_map()
    The caller has already passed in the memslot, and there are
    two instances `{kvm_faultin_pfn()/mark_page_dirty()}` of
    retrieving the memslot again in `kvm_riscv_gstage_map()`,
    we can replace them with existing KVM functions.
    
    Signed-off-by: Quan Zhou <zhouquan@iscas.ac.cn>

--- arch/riscv/kvm/mmu.c
+++ arch/riscv/kvm/mmu.c
@@ -648,7 +648,7 @@ int kvm_riscv_gstage_map(struct kvm_vcpu *vcpu,
 		return -EFAULT;
 	}
 
-	hfn = kvm_faultin_pfn(vcpu, gfn, is_write, &writable, &page);
+	hfn = __kvm_faultin_pfn(memslot, vcpu, gfn, is_write, &writable, &page);
 	if (hfn == KVM_PFN_ERR_HWPOISON) {
 		send_sig_mceerr(BUS_MCEERR_AR, (void __user *)hva,
 				vma_pageshift, current);
@@ -670,7 +670,7 @@ int kvm_riscv_gstage_map(struct kvm_vcpu *vcpu,
 		goto out_unlock;
 
 	if (writable) {
-		mark_page_dirty(kvm, gfn);
+		mark_page_dirty_in_slot(kvm, memslot, gfn);
 		ret = gstage_map_page(kvm, pcache, gpa, hfn << PAGE_SHIFT,
 				      vma_pagesize, false, true);
 	} else {
