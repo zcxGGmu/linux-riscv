e1b735e0b5d3b9de91bebcbee9fb482e85d22ace
Quan Zhou <zhouquan@iscas.ac.cn>
2025-11-28 15:41:10 +0800

    RISC-V: KVM: Add support for SBI_FWFT_PTE_AD_HW_UPDATING
    Add support in KVM SBI FWFT extension to allow VS-mode to request
    A/D Bit Hardware Update. A/D Bit Hardware Update can then be generated
    by VS-mode, allowing M-mode to redirect them to S-mode.
    
    Signed-off-by: Quan Zhou <zhouquan@iscas.ac.cn>

--- arch/riscv/include/uapi/asm/kvm.h
+++ arch/riscv/include/uapi/asm/kvm.h
@@ -234,6 +234,7 @@ struct kvm_riscv_sbi_fwft {
 	struct kvm_riscv_sbi_fwft_feature pointer_masking;
 	struct kvm_riscv_sbi_fwft_feature landing_pad;
 	struct kvm_riscv_sbi_fwft_feature shadow_stack;
+	struct kvm_riscv_sbi_fwft_feature pte_ad_hw_updating;
 };
 
 /* Possible states for kvm_riscv_timer */
--- arch/riscv/kvm/vcpu_sbi_fwft.c
+++ arch/riscv/kvm/vcpu_sbi_fwft.c
@@ -297,6 +297,48 @@ static long kvm_sbi_fwft_get_shadow_stack(struct kvm_vcpu *vcpu,
 	return SBI_SUCCESS;
 }
 
+static bool kvm_sbi_fwft_pointer_pte_ad_hw_updating_supported(struct kvm_vcpu *vcpu)
+{
+	return riscv_isa_extension_available(vcpu->arch.isa, SVADU) &&
+		!riscv_isa_extension_available(vcpu->arch.isa, SVADE);
+}
+
+static void kvm_sbi_fwft_reset_pte_ad_hw_updating(struct kvm_vcpu *vcpu)
+{
+	vcpu->arch.cfg.henvcfg &= ~ENVCFG_ADUE;
+}
+
+static long kvm_sbi_fwft_set_pte_ad_hw_updating(struct kvm_vcpu *vcpu,
+						   struct kvm_sbi_fwft_config *conf,
+						   bool one_reg_access, unsigned long value)
+{
+	struct kvm_vcpu_config *cfg = &vcpu->arch.cfg;
+
+	if (value == 1) {
+		cfg->henvcfg |= ENVCFG_ADUE;
+		if (!one_reg_access)
+			csr_write(CSR_HENVCFG, cfg->henvcfg);
+	} else if (value == 0) {
+		cfg->henvcfg &= ~ENVCFG_ADUE;
+		if (!one_reg_access)
+			csr_write(CSR_HENVCFG, cfg->henvcfg);
+	} else {
+		return SBI_ERR_INVALID_PARAM;
+	}
+
+	return SBI_SUCCESS;
+}
+
+static long kvm_sbi_fwft_get_pte_ad_hw_updating(struct kvm_vcpu *vcpu,
+						   struct kvm_sbi_fwft_config *conf,
+						   bool one_reg_access, unsigned long *value)
+{
+	struct kvm_vcpu_config *cfg = &vcpu->arch.cfg;
+
+	*value = (cfg->henvcfg & ENVCFG_ADUE) == ENVCFG_ADUE;
+	return SBI_SUCCESS;
+}
+
 static const struct kvm_sbi_fwft_feature features[] = {
 	{
 		.id = SBI_FWFT_MISALIGNED_EXC_DELEG,
@@ -335,7 +377,16 @@ static const struct kvm_sbi_fwft_feature features[] = {
 		.reset = kvm_sbi_fwft_reset_shadow_stack,
 		.set = kvm_sbi_fwft_set_shadow_stack,
 		.get = kvm_sbi_fwft_get_shadow_stack,
-	}
+	},
+	{
+		.id = SBI_FWFT_PTE_AD_HW_UPDATING,
+		.first_reg_num = offsetof(struct kvm_riscv_sbi_fwft, pte_ad_hw_updating.enable) /
+				 sizeof(unsigned long),
+		.supported = kvm_sbi_fwft_pte_ad_hw_updating_supported,
+		.reset = kvm_sbi_fwft_reset_pte_ad_hw_updating,
+		.set = kvm_sbi_fwft_set_pte_ad_hw_updating,
+		.get = kvm_sbi_fwft_get_pte_ad_hw_updating,
+	},
 };
 
 static const struct kvm_sbi_fwft_feature *kvm_sbi_fwft_regnum_to_feature(unsigned long reg_num)
